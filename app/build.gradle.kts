1c0434db2a3cc71b6ed8bc1e3fd39bd407cde6c8
unknown <denningbn@appstate.edu>
2023-10-13 16:15:41 -0400

    reqorked run page ui and added some locationrequest related code to runactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.further"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/further/RunActivity.java
+++ app/src/main/java/com/example/further/RunActivity.java
@@ -2,16 +2,40 @@ package com.example.further;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
 public class RunActivity extends AppCompatActivity {
 
+    TextView tv_location;
+
+    LocationRequest locationRequest;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
+
+        //ui elements
+        tv_location = findViewById(R.id.tv_location);
+
+        //create location client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        //create location request
+        locationRequest = new LocationRequest.Builder(locationRequest)
+                .setIntervalMillis(10000)
+                .setMinUpdateIntervalMillis(5000)
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .build();
 
     }
 }
