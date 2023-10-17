bc623dff9370b654626d45cc23695e5c68d69fd2
unknown <denningbn@appstate.edu>
2023-10-17 14:57:05 -0400

    added distinct updateGPS function, fixed locationRequest declaration, added onrequestPermissionsResults function, and added permissions stuff to androidmanifest.xml

--- app/src/main/java/com/example/further/RunActivity.java
+++ app/src/main/java/com/example/further/RunActivity.java
@@ -1,21 +1,26 @@
 package com.example.further;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class RunActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
     TextView tv_location;
 
     LocationRequest locationRequest;
@@ -29,20 +34,47 @@ public class RunActivity extends AppCompatActivity {
         //ui elements
         tv_location = findViewById(R.id.tv_location);
 
-        //create location client
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
 
         //create location request
-        locationRequest = new LocationRequest.Builder(locationRequest)
-                .setIntervalMillis(10000)
+        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,10000)
                 .setMinUpdateIntervalMillis(5000)
-                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
                 .build();
 
 
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                }
+                else {
+                    Toast.makeText(this,"This app requires permission to work properly." ,Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void updateGPS() {
+        //create location client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-        
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    tv_location.setText(location.toString());
+                }
+            });
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
         }
     }
 }
